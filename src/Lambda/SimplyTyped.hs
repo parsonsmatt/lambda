1c730d9f0c51c8668ecc46390c1e7676e3a094d3
Matt Parsons <parsonsmatt@gmail.com>
2016-02-05 17:51:40 -0500

    Omg such types

--- src/Lambda/SimplyTyped.hs
+++ src/Lambda/SimplyTyped.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 
 module Lambda.SimplyTyped where
 
-import Lambda.Common
+import Data.String
 
 -- | The simply typed lambda calculus begins with the untyped lambda calculus
 -- and adds types. Types have the following construction:
@@ -10,7 +15,67 @@ import Lambda.Common
 -- 1. A type variable, or
 -- 2. An arrow type
 
--- We represent these as follows:
+-- We represent these below. Rather than keeping a string or other identifier,
+-- we'll use the natural numbers to index types. This design decision is
+-- unfortunately caused by a limitation of Haskell's type system around what
+-- sorts of values can be lifted into the type level.
 data Type
-    = TyVar Variable
-    | Type :-> Type
+    = TyVar Nat
+    | TyArr Type Type
+    deriving (Show, Eq)
+
+instance IsString Type where
+    fromString = TyVar . read
+
+data Nat = Z | S Nat
+    deriving (Show, Eq, Read)
+
+type a :-> b = 'TyArr a b
+
+type LUnit = TyVar Z
+type LBool = TyVar (S Z)
+
+-- | Variables now contain a phantom type which indicates whether they are Type
+-- variables or Term variables.
+newtype Variable t = Variable String
+    deriving (Show, Eq)
+
+instance IsString (Variable t) where
+    fromString = Variable
+
+-- | Since we're dealing with types now, we have to be more clever with our
+-- representation of terms. The Haskell data type 'Term' takes a lifted data of
+-- kind Type and yields a value of kind *.
+--
+-- The constructor Var takes a Variable indexed on the type variable, and
+-- yields a Term indexed by the type given in the variable.
+--
+-- Application takes a term that has a type of `a :-> b` as the first thing,
+-- a `Term a` as the second thing, and returns a `Term b` as the final value.
+--
+-- Abstraction takes a Variable with type a, a Term of type b, and returns
+-- a Term of type `a -> b`.
+--
+-- While somewhat complex, this allows us to use Haskell's compiler to ensure
+-- that we don't construct invalid terms.
+data Term :: Type -> * where
+    Var :: Variable (Term a) -> Term a
+    App :: Term (a :-> b) -> Term a -> Term b
+    Abs :: Variable (Term a) -> Term b -> Term (a :-> b)
+
+deriving instance Show (Term n)
+
+instance IsString (Term a) where
+    fromString = Var . Variable
+
+x :: Term LUnit
+x = "x"
+
+y :: Term LBool
+y = Var (Variable "y")
+
+abs' :: Term (LUnit :-> LBool)
+abs' = Abs (Variable "x") (Var (Variable "y"))
+
+app' :: Term LBool
+app' = App abs' x
